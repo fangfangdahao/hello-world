7b09341c0a09617fd565c64d07abeffe35ac0d9e
fangfangdahao <72958794+fangfangdahao@users.noreply.github.com>
2020-10-16 14:20:48 +0800

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String helloWorld() {
+        return "hello world!";
+    }
+
+
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums = {-4,-1,0,3,10};
+        System.out.println(solution.helloWorld());
+    }
+}
